a5c20afe4827f9b39e89840b4cbadf40f852dcdd
nes2024 <nestor123uk@gmail.com>
2024-08-26 21:43:10 +0200

    initial full commit

---
+++ src/app/snippets/[id]/not-found.jsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl bold">
+        Sorry bruh, we could not find that goddam smippet
+      </h1>
+    </div>
+  );
+}
